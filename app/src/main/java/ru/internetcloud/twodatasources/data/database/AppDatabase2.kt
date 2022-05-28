e12f755fbfe7c2bc12c67fec378bb9848decf0a8
harisov-mobile <v8spec@mail.ru>
2022-05-28 11:11:23 +0300

    Create AppDatabase2.kt

---
+++ app/src/main/java/ru/internetcloud/twodatasources/data/database/AppDatabase2.kt
@@ -0,0 +1,42 @@
+package ru.internetcloud.twodatasources.data.database
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.internetcloud.twodatasources.data.entity.NoteDbModel
+
+@Database(entities = [NoteDbModel::class], version = 1, exportSchema = true)
+abstract class AppDatabase2 : RoomDatabase() {
+
+    abstract fun appDao(): AppDao
+
+    companion object {
+
+        private const val DATABASE_NAME = "notes2.db"
+
+        @Volatile // чтобы данная переменная не кэшировалась!!!
+        private var instance: AppDatabase? = null
+
+        private val Lock = Any()
+
+        fun getInstance(application: Application): AppDatabase {
+            instance?.let {
+                return it
+            }
+            synchronized(Lock) {
+                instance?.let {
+                    return it
+                }
+                val db = Room.databaseBuilder(
+                    application,
+                    AppDatabase::class.java,
+                    DATABASE_NAME
+                )
+                    .build()
+                instance = db
+                return db
+            }
+        }
+    }
+}
